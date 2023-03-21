b9becfff4afebb9323413b4defa15ea790f4683b
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-03-20 19:12:48 -0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,17 @@ import {
 import "./index.css";
 
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
+
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+
+import EditContact from "./routes/edit";
 
 
 const router = createBrowserRouter([
@@ -17,10 +26,17 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
