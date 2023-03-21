9863dccb71f47bf00f6c2b6f11b1461accd5858a
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-03-20 20:37:23 -0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import ErrorPage from "./error-page";
 
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 
 import Root, {
@@ -17,7 +18,14 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 
-import EditContact from "./routes/edit";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+
+import { action as destroyAction } from "./routes/destroy";
+
+
+import Index from "./routes/index";
 
 
 const router = createBrowserRouter([
@@ -29,15 +37,29 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-      },
+        errorElement: <ErrorPage />,
+        children: [
+
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
+      }
     ],
   },
 ]);
