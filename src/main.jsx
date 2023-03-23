d775bf78b6974be0cd783347d1a341ca23f14dbd
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-03-22 19:17:45 -0600

    react-router

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
       {
         errorElement: <ErrorPage />,
         children: [
-
+            
           { index: true, element: <Index /> },
           {
             path: "contacts/:contactId",
